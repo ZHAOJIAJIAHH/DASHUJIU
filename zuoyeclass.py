728b12a08776f3d9f8e26714c1d74f3285273d61
ZHAOJIAJIAHH <39938210+ZHAOJIAJIAHH@users.noreply.github.com>
2019-08-21 08:45:34 +0800

    Add files via upload

--- zuoyeclass.py
+++ zuoyeclass.py
@@ -1,3 +1,97 @@
+'''
+#第一题：
+class Rectangle(object):
+    def __init__(self,width = 1,height = 2):
+        self.width = width
+        self.height = height
+
+    def getArea(self):
+        return self.width*self.height
+
+    def getPerimeter(self):
+        return (self.width+self.height)*2
+
+    def main(self):
+        rectangle = Rectangle(4,40)
+        print('宽是>>',rectangle.width)
+        print('高是>>',rectangle.height)
+        print('面积是>>',rectangle.getArea())
+        print('周长是>>',rectangle.getPerimeter())
+        print('---------------------------------')
+        tom = Rectangle(3.5,35.7)
+        print('宽是>>',tom.width)
+        print('高是>>',tom.height)
+        print('面积是>>',tom.getArea())
+        print('周长是>>',tom.getPerimeter())
+
+if __name__ == "__main__":
+    x = Rectangle()
+    x.main()
+'''
+'''
+#第二题
+class Account(object):
+    def __init__(self, id_=0, balance=100.0, annualInterestRate=0.0):
+        self._id = id_
+        self._balance = balance
+        self._annualInterestRate = annualInterestRate
+
+@property
+def id(self):
+    return self._id
+
+@property
+def balance(self):
+    return self._balance
+
+@property
+def annualInterestRate(self):
+    return self._annualInterestRate
+
+@id.setter
+def id(self, new_id):
+    self._id = new_id
+
+@balance.setter
+def balance(self, new_balance):
+    self._balance = new_balance
+
+@annualInterestRate.setter
+def annuallInterestRate(self, new_annualInterestRate):
+    self._annuallInterestRate = new_annualInterestRate
+
+def getMonthlyInterestRate(self):
+    return self._annualInterestRate/12/100
+
+def getMonthlyInterest(self):
+    rate = self.getMonthlyInterestRate()
+    res = self._balance * rate
+    return res
+
+def withdraw(self, money):
+    if self._balance >= money:
+        self._balance -= money
+        print("您已成功取出：%.2f" % money)
+        print("您的余额为：%.2f" % self._balance)
+    else:
+    print("您的余额不足")
+    print("您的余额为：%.2f" % self._balance)
+def deposit(self, money):
+    self._balance += money
+    print("您已成功存入：%.2f" % money)
+    print("您的余额为：%.2f" % self._balance)
+
+
+if __name__ == "__main__":
+    a = Account(id_=1122, balance=20000, annualInterestRate=4.5)
+    a.withdraw(2500)
+    a.deposit(3000)
+    print("月利率为：",a.getMonthlyInterestRate())
+    print("月利息为：",a.getMonthlyInterest())
+
+'''
+
+
 """
 #第三题
 class Fan(object):
@@ -48,93 +142,57 @@ if __name__ =="__main__":
     print("颜色：",fan1.color)
     print("已打开：",fan1.on)
 """
-'''
-#第一题：
-class Rectangle(object):
-    def __init__(self,width = 1,height = 2):
-        self.width = width
-        self.height = height
 
+import math
+
+class RegularPolygon(object):
+    def __init__(self,n = 3,side = 1,x = 0,y = 0):
+        self._n = n
+        self._side = side
+        self._x = x
+        self._y = y
+
+    @property #装饰器
+    def n(self):
+        return self._n  
+    @n.setter #修改器
+    def n(self,n1):
+        self._n = n1
+    
+    @property #装饰器
+    def side(self):
+        return self._side 
+    @side.setter #修改器
+    def side(self,side1):
+        self._side = side1
+    
+    @property #装饰器
+    def x(self):
+        return self._x 
+    @x.setter #修改器
+    def x(self,x1):
+        self._x = x1
+    
+    @property #装饰器
+    def y(self):
+        return self._y 
+    @y.setter #修改器
+    def y(self,y1):
+        self._y = y1
+    def getPerimerter(self):
+        return self.n*self.side
     def getArea(self):
-        return self.width*self.height
-
-    def getPerimeter(self):
-        return (self.width+self.height)*2
-
-    def main(self):
-        rectangle = Rectangle(4,40)
-        print('宽是>>',rectangle.width)
-        print('高是>>',rectangle.height)
-        print('面积是>>',rectangle.getArea())
-        print('周长是>>',rectangle.getPerimeter())
-        print('---------------------------------')
-        tom = Rectangle(3.5,35.7)
-        print('宽是>>',tom.width)
-        print('高是>>',tom.height)
-        print('面积是>>',tom.getArea())
-        print('周长是>>',tom.getPerimeter())
-
+        return self.n * self.side ** 2 / (4 * math.tan(math.pi / self.n))
 if __name__ == "__main__":
-    x = Rectangle()
-    x.main()
-'''
-
-'''
-import multiprocessing
-import json 
-import os
-import requests
-def dy():
-    with open('C:\\Users\\asus\\Desktop\\top_500.txt',mode='r') as f:
-        res = f.readlines()[0].strip('\n').split('}')
-        # count = 0
-        list_1 = []
-        for json_ in res[:-1]:
-            _json = json_ + '}'
-            _json = json.loads(_json)
-            song_play_url = _json['song_play_url']
-            song_name = _json['song_name']
-            print (song_name)
-            if song_play_url is not None:
-                list_1.append(song_play_url)
-                # count +=1
-                # print(song_play_url)
-                # print(count)
-                for i in list_1[0:24]:
-                    path = i
-                    response = requests.get(path)
-                    mp3_ = response.content
-                    with open('C:\\Users\\asus\\Desktop\\music\\' +song_name+'.mp3',mode='wb') as f:
-                        f.write(mp3_)
-def dx():
-    with open('C:\\Users\\asus\\Desktop\\top_500.txt',mode='r') as f:
-        res = f.readlines()[0].strip('\n').split('}')
-        # count = 0
-        list_1 = []
-        for json_ in res[:-1]:
-            _json = json_ + '}'
-            _json = json.loads(_json)
-            song_play_url = _json['song_play_url']
-            song_name = _json['song_name']
-            #print (song_name)
-            if song_play_url is not None:
-                list_1.append(song_play_url)
-                # count +=1
-                # print(song_play_url)
-                # print(count)
-                for i in list_1[24:47]:
-                    path = i
-                    response = requests.get(path)
-                    mp3_ = response.content
-                    with open('C:\\Users\\asus\\Desktop\\music\\' +song_name+'.mp3',mode='wb') as f:
-                        f.write(mp3_)
-        
-if __name__ == '__main__':
-    p1 = multiprocessing.Process(target=dy)   
-    p2 = multiprocessing.Process(target=dx)
-    p1.start()#启动线程
-    p2.start()
-    p1.join() #阻塞线程
-    p2.join()
-    print('ok')
-'''
+    rp1 = RegularPolygon()
+    print("周长为:",'%.2f'%rp1.getPerimerter())
+    print("面积为:",'%.2f'%rp1.getArea())
+    print("---------------------------------------")
+    rp2 = RegularPolygon(6,4)
+    print("周长为:",'%.2f'%rp2.getPerimerter())
+    print("面积为:",'%.2f'%rp2.getArea())
+    print("---------------------------------------")
+    rp3 = RegularPolygon(10,4,5.6,7.8)
+    print("周长为:",'%.2f'%rp3.getPerimerter())
+    print("面积为:",'%.2f'%rp3.getArea())
+    
